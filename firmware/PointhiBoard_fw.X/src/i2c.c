b494c6097c0b4a17bd813f27b7736fb63b5151a9
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2014-04-12 17:58:45 +0200

    Initial working I2C-Implementation

--- firmware/PointhiBoard_fw.X/src/i2c.c
+++ firmware/PointhiBoard_fw.X/src/i2c.c
@@ -14,130 +14,110 @@
 #include "define.h"
 #include "io.h"
 
+unsigned char I2cRegisterIndex = 0;
+unsigned char I2cInterruptFrameIndex = 0;
+unsigned char I2cInterruptI2cAdress = 0;
+
 void I2cInit() {
+    // Set I2C-Adress
+    SSPADD = I2C_DEFAULT_ADRESS; 
 
-    SSPADD = I2C_DEFAULT_ADRESS; // Set Adress
+//    IoSetPortBTris(0x00);
+//    IoSetPortB(I2cRegisterIndex);
 
+    // Clear SSPSTAT
     SSPSTAT = 0x00;
 
-    //    SSPSTATbits.SMP = 1;        // Slew Rate Control disabled for Standard Speed mode
+    SSPSTATbits.SMP = 1;        // Slew Rate Control disabled for Standard Speed mode
     //    SSPSTATbits.CKE = 1;        // Enable SMBus specific inputs
 
+    // I2C Slave mode, 7-bit adress
+    SSPCON1 = 0x06; 
 
-    SSPCON1 = 0x00;
-    //    SSPCON1 &= 0x2F;
-
-    //    SSPCON1 |= 0x0E;            // Slave mode, 7-bit adress with Start and Stop bit interrupts enabled
-    SSPCON1 |= 0x06; // Slave mode, 7-bit adress
-
-    SSPCON1bits.SSPEN = 1; // Enables the mssp Port
-
+    // Clear SSPCON2
     SSPCON2 = 0x00;
+    
+    // Enable Clock stretching
+    SSPCON2bits.SEN = 1;
 
-    //    SSPCON2bits.GCEN = 1;       // Enable interrupt on general adress
-    //    SSPCON2bits.SEN = 1;        // Clock Streching is enabled for transmit and receive
+    // Enable interrupt on general adress
+    //SSPCON2bits.GCEN = 1;
+
+    // Enables I2C Port
+    SSPCON1bits.SSPEN = 1; 
 }
 
 inline void I2cInterrupt() {
-    if (SSPSTATbits.BF) // New Byte recive
-    {
-        SSPSTATbits.BF = 0;
-        if (!SSPSTATbits.R_NOT_W) // Master Write Data
-        {
-            IoSetLedStat(LED_ON);
-            if (SSPSTATbits.D_NOT_A) // Last recived Byte was Data
-            {
-                unsigned char I2cHelp = SSPBUF;
-                SSPBUF = I2cHelp;
-                //                switch(I2cRegisterSelect)
-                //                    {
-                ////                    case 0:
-                ////                        break;
-                //                    case 1: // 1. Engine
-                //                        if(I2cHelp == 128)
-                //                            {
-                //                            Engine1Stop();
-                //                            }
-                //                        else if(I2cHelp > 128)
-                //                            {
-                //                            Engine1Forward();
-                //                            Engine1Pwm((I2cHelp-128)*2);
-                //                            }
-                //                        else
-                //                            {
-                //
-                //                            Engine1Backward();
-                //                            Engine1Pwm((128-I2cHelp)*2);
-                //                            }
-                //                        break;
-                //                    case 2:
-                //                        break;
-                //                    case 3: // 2. Engine
-                //                        if(I2cHelp == 128)
-                //                            {
-                //                            Engine2Stop();
-                //                            }
-                //                        else if(I2cHelp > 128)
-                //                            {
-                //                            Engine2Forward();
-                //                            Engine2Pwm((I2cHelp-128)*2);
-                //                            }
-                //                        else
-                //                            {
-                //                            Engine2Backward();
-                //                            Engine2Pwm((128-I2cHelp)*2);
-                //                            }
-                //                        break;
-                //                    case 4:
-                //                        break;
-                //                    default:
-                //                        break;
-                //                    }
-                //                I2cRegisterSelect ++;
-            } else // Last recived Byte was Adress
-            {
-                //                I2cRegisterSelect = SSPBUF; // Update Register Select
-                //                I2cRegisterSelect = 0;
-            }
+    // Workaround, to decrement Register-Index at next I2c-Request if necessary
+    static unsigned char DecrementI2cRegisterIndex = 0;
 
-            SSPCON1bits.CKP = 1;
-        } else // Master Request Data
-        {
-            IoSetLedInfo(LED_ON);
-            if (!SSPSTATbits.D_NOT_A) // Last recived Byte was Adress
-            {
-                //                I2cRegisterSelect = 0; // Update Register Select
-            }
-            SSPCON1bits.SSPOV = 0;
-            SSPCON1bits.WCOL = 0;
-            //            I2cHelp = SSPBUF;
-            //                I2cRegisterSelect = 0xFF;
-            //                SSPBUF = I2cRegisterSelect;
-
-            //            switch(I2cRegisterSelect)
-            //                {
-            //                case 0:
-            SSPBUF = 0x00;
-            //                    break;
-            //                case 1:
-            //
-            //
-            //                    break;
-            //
-            //                case 2:
-            //
-            //
-            //                    break;
-            //
-            //                default:
-            //                    SSPBUF = 0x00;
-            //                    break;
-            //                }
-
-            //            I2cRegisterSelect ++;
-            SSPCON1bits.CKP = 1;
+    if (!SSPSTATbits.D_NOT_A) {
+        // Last recived Byte was Adress
+        // Clear BF and get I2c-Adress
+        I2cInterruptI2cAdress = SSPBUF;
+        I2cInterruptFrameIndex = 0;
+        if(DecrementI2cRegisterIndex) {
+            I2cRegisterIndex --;
+            DecrementI2cRegisterIndex = 0;;
         }
+    } else {
+        // Last recived Byte was Data
+        I2cInterruptFrameIndex ++;
     }
-    SSPCON1bits.CKP = 1;
-//    PIR1bits.SSPIF = 0; // Reset Interrupt-Byte
+
+//    if(SSPCON1bits.SSPOV ) {
+//        IoSetLedInfo(LED_BLINK_SLOW);
+//        SSPCON1bits.SSPOV = 0;
+//    }
+//
+//    if(SSPCON1bits.WCOL) {
+//        IoSetLedStat(LED_BLINK_SLOW);
+//        SSPCON1bits.WCOL = 0;
+//    }
+
+    if (SSPSTATbits.R_NOT_W) {
+        // Master Read Data from Slave
+        
+        // Perform Action depending on RegisterIndex
+        switch(I2cRegisterIndex) {
+            default:
+                SSPBUF = I2cRegisterIndex;
+                break;
+        }
+
+        // Set next Register-Index
+        I2cRegisterIndex ++;
+        // Workaround, to decrement the last increment of RegisterIndex
+        DecrementI2cRegisterIndex = 1;
+
+        // Release Clock
+        SSPCON1bits.CKP = 1;
+    }
+    else {
+        // Master Write Data from Slave
+
+        if (SSPSTATbits.D_NOT_A) {
+            // Last recived Byte was Data
+            if(I2cInterruptFrameIndex == 1) {
+                // Update Register Index
+                I2cRegisterIndex = SSPBUF; 
+            } else {
+                // Perform Action depending on RegisterIndex
+                switch(I2cRegisterIndex) {
+                    default: {
+                        unsigned char I2cDelRegister = SSPBUF;
+                        }
+                        break;
+                }
+
+                // Set next Register-Index
+                I2cRegisterIndex ++;
+            }
+        }
+
+        // Release Clock
+        SSPCON1bits.CKP = 1;
+    }
+
+//    IoSetPortB(I2cRegisterIndex);
 }
--- firmware/PointhiBoard_fw.X/src/interrupt.c
+++ firmware/PointhiBoard_fw.X/src/interrupt.c
@@ -27,37 +27,37 @@ void InterruptInit() {
     InterruptInitAdc();
     InterruptInitI2C();
     
-    PEIE = 1; // Aktiviere Pheripherale Interrupts
-    IPEN    = 1;    // Aktiviere Prioritäten
-    GIE = 1; // Aktiviere Globale Interrupt
+    INTCONbits.PEIE = 1; // Aktiviere Pheripherale Interrupts
+    RCONbits.IPEN    = 1;    // Aktiviere Prioritäten
+    INTCONbits.GIE = 1; // Aktiviere Globale Interrupt
 }
 
 void InterruptInitTimer0() {
     // Timer0 Registers:// 16-Bit Mode; Prescaler=1:4; TMRH Preset=B1; TMRL Preset=E0; Freq=50,00Hz; Period=20.000.000 ns
-    T0CON, TMR0ON = 1; // Timer0 On/Off Control bit:1=Enables Timer0 / 0=Stops Timer0
-    T0CON, T08BIT = 0; // Timer0 8-bit/16-bit Control bit: 1=8-bit timer/counter / 0=16-bit timer/counter
-    T0CON, T0CS = 0; // TMR0 Clock Source Select bit: 0=Internal Clock (CLKO) / 1=Transition on T0CKI pin
-    T0CON, T0SE = 0; // TMR0 Source Edge Select bit: 0=low/high / 1=high/low
-    T0CON, PSA = 0; // Prescaler Assignment bit: 0=Prescaler is assigned; 1=NOT assigned/bypassed
-    T0CON, T0PS2 = 0; // bits 2-0  PS2:PS0: Prescaler Select bits
-    T0CON, T0PS1 = 0;
-    T0CON, T0PS0 = 1;
+    T0CONbits.TMR0ON = 1; // Timer0 On/Off Control bit:1=Enables Timer0 / 0=Stops Timer0
+    T0CONbits.T08BIT = 0; // Timer0 8-bit/16-bit Control bit: 1=8-bit timer/counter / 0=16-bit timer/counter
+    T0CONbits.T0CS = 0; // TMR0 Clock Source Select bit: 0=Internal Clock (CLKO) / 1=Transition on T0CKI pin
+    T0CONbits.T0SE = 0; // TMR0 Source Edge Select bit: 0=low/high / 1=high/low
+    T0CONbits.PSA = 0; // Prescaler Assignment bit: 0=Prescaler is assigned; 1=NOT assigned/bypassed
+    T0CONbits.T0PS2 = 0; // bits 2-0  PS2:PS0: Prescaler Select bits
+    T0CONbits.T0PS1 = 0;
+    T0CONbits.T0PS0 = 1;
 
-    TMR0IP = 1; // Timer 0 ... High Priority
-    TMR0IE = 1; // Activate Interrupt for Timer0
+    INTCON2bits.TMR0IP = 0; // Timer 0 ... Low Priority
+    INTCONbits.TMR0IE = 1; // Activate Interrupt for Timer0
 }
 
 void InterruptInitAdc() {
-    PIE1bits.ADIE = 1; // Aktiviere Interrupt für ADC
     IPR1bits.ADIP = 0; // Low Priority
-
+    PIE1bits.ADIE = 1; // Aktiviere Interrupt für ADC
+    
     // Start Endless ADC-Call
-    ADCON0bits.GO_nDONE = 1; // Start ADC
+    ADCON0bits.GO_nDONE = 1;
 }
 
 void InterruptInitI2C() {
-    PIE1bits.SSPIE = 1; // Aktiviere Interrupt für MSSP-Modul
-    IPR1bits.SSPIP = 0; // Low Priority
+    IPR1bits.SSPIP = 1; // High Priority
+    PIE1bits.SSPIE = 1; // Activate Interrupt for MSSP-Port
 }
 
 inline void InterruptOfTimer0() {
@@ -67,24 +67,24 @@ inline void InterruptOfTimer0() {
 }
 
 void interrupt tc_int(void) {
-    if (TMR0IF == 1) // Interrupt, Timer0
-    {
-        // Config 50Hz Mode
-        TMR0H = 0xB1; // preset for Timer0 MSB register
-        TMR0L = 0xE0; // preset for Timer0 LSB register
-        TMR0IF = 0;
-        InterruptOfTimer0();
+    if (PIR1bits.SSPIF == 1 && PIE1bits.SSPIE == 1) {
+        // I2C-Interrupt
+        I2cInterrupt();
+        PIR1bits.SSPIF = 0;
     }
 }
 
 void interrupt low_priority tc_clr(void) {
-    if (PIR1bits.SSPIF == 1 && PIE1bits.SSPIE == 1) // I2C-Interrupt    // Help, when Chip error
-    {
-        PIR1bits.SSPIF = 0;
-        I2cInterrupt();
+    if (TMR0IF == 1) {
+        // Interrupt, Timer0
+        // Config 50Hz Mode
+        TMR0H = 0xB1; // preset for Timer0 MSB register
+        TMR0L = 0xE0; // preset for Timer0 LSB register
+        INTCONbits.TMR0IF = 0;
+        InterruptOfTimer0();
     }
-    if (PIR1bits.ADIF == 1 && PIE1bits.ADIE == 1) // ADC-Interrupt
-    {
+    if (PIR1bits.ADIF == 1 && PIE1bits.ADIE == 1) {
+        // ADC-Interrupt
         PIR1bits.ADIF = 0;
         AdcInterrupt();
     }
